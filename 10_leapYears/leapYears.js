da021c7b8dcd1192dafe1e1fd6d5e7b81e556692
Tom <74308275+tjturner9@users.noreply.github.com>
2025-08-04 05:26:12 +0000

    loops and arrays complete

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearDivByFour = year % 4 === 0;
+    const isYearCent = year % 100 === 0;
+    const isYearFourHund = year % 400 === 0;
 
-};
+    if (isYearDivByFour && 
+        (!isYearCent || isYearFourHund)
+    ) {
+        return true
+    } else {
+        return false
+    }
+}
+
 
 // Do not edit below this line
 module.exports = leapYears;
